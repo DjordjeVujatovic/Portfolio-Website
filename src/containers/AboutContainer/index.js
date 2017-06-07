e8282ffec74a67a811da7e97e3bb95a48d338538
Djordje Vujatovic <djordje_v@hotmail.com>
2017-06-07 11:29:56 -0700

    Created Projects and About containers.

---
+++ src/containers/AboutContainer/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AboutContainer extends Component {
+  render() {
+    return (
+      <div>
+        <p>About Container</p>
+      </div>
+    );
+  }
+}
+
+export default AboutContainer;
