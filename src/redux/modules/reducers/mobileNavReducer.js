9dbc3c9399d79fc0d6a2603ea722662fbfae6268
Djordje Vujatovic <djordje_v@hotmail.com>
2017-06-08 11:38:06 -0700

    Wrote reducer for mobileNav state.

---
+++ src/redux/modules/reducers/mobileNavReducer.js
@@ -0,0 +1,17 @@
+import { EXPAND, CLOSE_EXPAND } from '../actions/mobileNavActions';
+
+const initialState = {
+  displayExpand: false,
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case EXPAND:
+      return { ...state, displayExpand: true };
+    case CLOSE_EXPAND:
+      return { ...state, displayExpand: false };
+    default:
+      return state;
+  }
+};
+
