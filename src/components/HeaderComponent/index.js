6581158ccc3662d64f9b7e57d0ea52166411779c
Djordje Vujatovic <djordje_v@hotmail.com>
2017-06-15 15:53:14 -0700

    Added header text.

--- src/components/HeaderComponent/index.js
+++ src/components/HeaderComponent/index.js
@@ -3,9 +3,18 @@ import React from 'react';
 const HeaderComponent = () => {
   return (
     <div className="headerComponent">
-      <div className="headerButtonContainer">
-        <button className="gitHubButton">GitHub</button>
-        <button className="linkedInButton">LinkedIn</button>
+      <div className="headerContainer">
+        <div className="headerWrapper">
+          <div className="headerTextWrapper">
+            <div>
+              <p>Making the commitment to become a software developer is making a commitment to lifelong learning</p>
+            </div>
+          </div>
+          <div className="headerButtonContainer">
+            <button className="gitHubButton">GitHub</button>
+            <button className="linkedInButton">LinkedIn</button>
+          </div>
+        </div>
       </div>
     </div>
   );
