e57cdbb627a7f1bf88095b55fed1b4c9cc5b5c3c
Djordje Vujatovic <djordje_v@hotmail.com>
2017-06-14 22:27:51 -0700

    Added small details to components.

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -18,9 +18,9 @@ class App extends Component {
         <AboutContainer />
         <EducationHero />
         <EducationContainer />
+        <BooksContainer />
         <ToolsHero />
         <ToolsContainer />
-        <BooksContainer />
         <ProjectContainer />
       </div>
     );
