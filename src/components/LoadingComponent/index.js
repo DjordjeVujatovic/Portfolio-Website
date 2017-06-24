94eaa57c328e44967985b223732f9e8af9808d39
Djordje Vujatovic <djordje_v@hotmail.com>
2017-06-24 14:46:32 -0700

    Added fun loading Gif.

--- src/components/LoadingComponent/index.js
+++ src/components/LoadingComponent/index.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 const LoadingComponent = () => {
   return (
-    <div>
-      Loading...
+    <div className="loadingContainer">
+      <div className="lds-css ng-scope">
+        <div className="lds-ripple">
+          <div />
+          <div />
+        </div>
+      </div>
     </div>
   );
 };
