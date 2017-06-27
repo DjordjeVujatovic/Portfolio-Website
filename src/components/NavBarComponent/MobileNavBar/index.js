4bb48847643c25795a3e5dabb3280d60c1c9eb08
Djordje Vujatovic <djordje_v@hotmail.com>
2017-06-27 12:44:09 -0700

    Fixing height issues on safari for tools section.

--- src/components/NavBarComponent/MobileNavBar/index.js
+++ src/components/NavBarComponent/MobileNavBar/index.js
@@ -3,8 +3,20 @@ import PropTypes from 'prop-types';
 
 const MobileNavBar = ({ expand }) => (
   <div className="mobileNavBar">
-    <p>Djordje Vujatovic</p>
+    <div className="nameContainer">
+      <p>Djordje Vujatovic</p>
+    </div>
     <button className="openButton" onClick={() => expand()}><i className="fa fa-bars fa-2x" aria-hidden="true" /></button>
+    <div className="fullScreenNav">
+      <div className="fullScreenButtonContainer">
+        <button>About</button>
+        <button>Education</button>
+        <button>Favorite Books</button>
+        <button>Tool Box</button>
+        <button>Projects</button>
+        <button>Find Me</button>
+      </div>
+    </div>
   </div>
 );
 
