152ca351ee5ad8b71218e571062e96d0d7512b83
Djordje Vujatovic <djordje_v@hotmail.com>
2017-07-05 15:49:32 -0700

    Removed redunant former src folder.

--- testing/src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import thunk from 'redux-thunk';
-import combinedReducers from './reducers';
-
-export default createStore(
-  combinedReducers,
-  composeWithDevTools(applyMiddleware(thunk)),
-);
